061cc4c6e2aac73fd4e7de02682d57b70310ebfe
S M Al Hasib <alhasibsm@gmail.com>
2022-10-21 18:10:54 +0600

    Updated Manjaro script

--- manjaro.sh
+++ manjaro.sh
@@ -1,40 +1,24 @@
-#!/bin/bash
-sudo pacman -Syyuu
+#!/usr/bin/zsh
+echo Installing necessary packages from pacman repo
+sudo pacman -Syu --noconfirm yay base-devel nodejs lolcat screenfetch neofetch simplescreenrecorder cmatrix freeglut glfw glew jdk-openjdk qbittorrent vlc vim xclip kdenlive tldr qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat cmake ninja
+sudo pacman -S ebtables iptables libguestfs
 
-sudo pacman -Syu --noconfirm yay base-devel git wget curl v4l-utils snapd pamac vlc jre11-openjdk jdk11-openjdk java11-openjfx jre8-openjdk jdk8-openjdk java8-openjfx gcc nodejs npm gimp flameshot okular qbittorrent kdenlive screenfetch
-sudo systemctl enable --now snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
+echo tweaks and manuals
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
+sudo sed -i '/unix_sock_group/s/^#//' /etc/libvirt/libvirtd.conf
+sudo sed -i '/unix_sock_rw_perms/s/^#//' /etc/libvirt/libvirtd.conf
+sudo usermod -a -G libvirt $(whoami)
+newgrp libvirt
+sudo systemctl restart libvirtd.service
+curl -s https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh && sudo chmod +x /usr/local/bin/cht.sh
 git config --global user.name "S M Al Hasib"
-git config --global user.email "smalreal14@gmail.com"
-sudo npm i -g tldr
+git config --global user.email "alhasibsm@gmail.com"
+tldr -u
+echo -e "LABEL=Work /mnt/Work auto nosuid,nodev,nofail,x-gvfs-show 0 0\nLABEL=Entertainment /mnt/Entertainment auto nosuid,nodev,nofail,x-gvfs-show 0 0\nLABEL=Whatever /mnt/Whatever auto nosuid,nodev,nofail,x-gvfs-show 0 0" | sudo tee -a /etc/fstab > /dev/null
 
-echo Installing Google Chrome
-yay -Syu google-chrome teamviewer jdk kotlin indicator-stickynotes android-studio intellij-idea-community-edition webstorm
+echo Installing AUR repo2
+yay -Syu --noconfirm --answerdiff None --answerclean All google-chrome teamviewer discord jetbrains-toolbox zoom teams visual-studio-code-bin ventoy flutter spotify ganache-bin
+export CHROME_EXECUTABLE=google-chrome-stable
 
-sudo pacman -Syu --noconfirm chrome-gnome-shell
-
-wget https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz
-tar -xf xdm-setup-7.2.11.tar.xz
-sudo ./install.sh
-
-#sudo pacman -Syu --noconfirm jre-openjdk jdk-openjdk java-openjfx
-
-yay -Syu --noconfirm mongodb
-
-sudo snap install --classic code
-code --install-extension formulahendry.code-runner --force
-code --install-extension ms-vscode.cpptools --force
-code --install-extension austin.code-gnu-global --force
-code --install-extension esbenp.prettier-vscode --force
-code --install-extension msjsdiag.debugger-for-chrome --force
-code --install-extension ms-python.python --force
-code --install-extension redhat.java --force
-code --install-extension dbaeumer.vscode-eslint --force
-code --install-extension vscode-icons-team.vscode-icons --force
-code --install-extension VisualStudioExptTeam.vscodeintellicode --force
-
-sudo snap install pycharm-professional --classic
-
-yay -Syu --noconfirm howdy
-
-sudo snap install intellij-idea-community --classic
+yay -Scc --noconfirm
