9a1050197be58a3ad4b2ff6744dfb02b0dacfd53
S M Al Hasib <alhasibsm@gmail.com>
2022-11-03 10:08:55 +0600

    Manjaro script 1.3

--- manjaro.sh
+++ manjaro.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/zsh
-echo Installing necessary packages from pacman repo
-sudo pacman -Syu --noconfirm yay base-devel nodejs lolcat screenfetch neofetch simplescreenrecorder cmatrix freeglut glfw glew jdk-openjdk qbittorrent vlc vim xclip kdenlive tldr qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat cmake ninja translate-shell
+echo 'Installing necessary packages from pacman repo'
+sudo pacman -Syu --noconfirm yay base-devel nodejs lolcat screenfetch neofetch simplescreenrecorder cmatrix freeglut glfw glew jdk-openjdk qbittorrent vlc vim xclip kdenlive tldr qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat cmake ninja translate-shell discord npm
 sudo pacman -S ebtables iptables libguestfs
 
-echo tweaks and manuals
+echo 'tweaks and manuals'
 sudo systemctl enable libvirtd.service
 sudo systemctl start libvirtd.service
 sudo sed -i '/unix_sock_group/s/^#//' /etc/libvirt/libvirtd.conf
@@ -18,9 +18,14 @@ tldr -u
 echo -e "LABEL=Work /mnt/Work auto nosuid,nodev,nofail,x-gvfs-show 0 0\nLABEL=Entertainment /mnt/Entertainment auto nosuid,nodev,nofail,x-gvfs-show 0 0\nLABEL=Whatever /mnt/Whatever auto nosuid,nodev,nofail,x-gvfs-show 0 0" | sudo tee -a /etc/fstab > /dev/null
 echo -e 'alias ban='\''trans en:bn'\' | tee -a .zshrc > /dev/null
 
-echo Installing AUR repo2
-yay -Syu --noconfirm --answerdiff None --answerclean All google-chrome teamviewer discord jetbrains-toolbox zoom teams visual-studio-code-bin ventoy flutter spotify ganache-bin
+echo 'Installing AUR repo2'
+for package in google-chrome teamviewer jetbrains-toolbox zoom teams visual-studio-code-bin ventoy flutter spotify ganache-bin
+do
+    yay -Syu --noconfirm --answerdiff None --answerclean All package
+done
+
 export CHROME_EXECUTABLE=google-chrome-stable
 sudo gpasswd -a ${USER} flutterusers
 
+echo 'Clearing cache'
 yay -Scc --noconfirm
